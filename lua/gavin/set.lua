cec1c5320787a139c9951e629dcdd29e603ad74a
Gavin <gavin.speek@keypoint.eu>
2025-03-04 16:00:49 +0100

    move yank highlight to logischere plek

--- lua/gavin/set.lua
+++ lua/gavin/set.lua
@@ -15,4 +15,10 @@ vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 
-
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
